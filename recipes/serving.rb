58043bbc04ed4f9e2987a786ff0e759c0af8464a
Alex Ormenisan <ormenisan.adrian@gmail.com>
2019-07-15 10:54:33 +0200

    temp commit - fix ubuntu tensorflow key expired

--- recipes/serving.rb
+++ recipes/serving.rb
@@ -6,14 +6,15 @@ when "debian"
     user "root"
     #ignore_failure true
     code <<-EOF
-      echo "deb [arch=amd64] http://storage.googleapis.com/tensorflow-serving-apt testing tensorflow-model-server tensorflow-model-server-#{node['tensorflow']['serving']['version']}" | sudo tee /etc/apt/sources.list.d/tensorflow-serving.list
-      curl https://storage.googleapis.com/tensorflow-serving-apt/tensorflow-serving.release.pub.gpg | sudo apt-key add -        
-      add-apt-repository ppa:ubuntu-toolchain-r/test -y
-      apt-get update
+      #echo "deb [arch=amd64] http://storage.googleapis.com/tensorflow-serving-apt testing tensorflow-model-server tensorflow-model-server-#{node['tensorflow']['serving']['version']}" | sudo tee /etc/apt/sources.list.d/tensorflow-serving.list
+      #curl https://storage.googleapis.com/tensorflow-serving-apt/tensorflow-serving.release.pub.gpg | sudo apt-key add -        
+      #add-apt-repository ppa:ubuntu-toolchain-r/test -y
+      #apt-get update
     EOF
   end
 
-  package ["libstdc++6", "tensorflow-model-server"]
+  #package ["libstdc++6", "tensorflow-model-server"]
+  package ["libstdc++6"]
 
 when "rhel"
   package ["curl", "libcurl", "git", "freetype-devel", "libpng12-devel", "python2-pkgconfig", "python-devel", "python2-pip", "swig", "zip", "zlib-devel", "giflib-devel", "zeromq3-devel"]
